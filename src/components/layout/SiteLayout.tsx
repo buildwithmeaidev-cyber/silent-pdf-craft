ed3ec24eac7d7c49bcd3c8005218ab6a0b532a7f
buildwithmeaidev-cyber <buildwithme.ai.dev@gmail.com>
2026-05-13 19:20:18 +0530

    Restore vertical page scrolling

--- src/components/layout/SiteLayout.tsx
+++ src/components/layout/SiteLayout.tsx
@@ -3,9 +3,9 @@ import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
 
 export const SiteLayout = () => (
-  <div className="min-h-screen w-full max-w-full overflow-x-hidden flex flex-col">
+  <div className="min-h-screen w-full max-w-full flex flex-col">
     <Navbar />
-    <main className="flex-1 w-full max-w-full overflow-x-hidden">
+    <main className="flex-1 w-full max-w-full">
       <Outlet />
     </main>
     <Footer />
