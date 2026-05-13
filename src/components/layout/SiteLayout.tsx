6822cf2041c9b2e547f088e6c9af06675782282a
Bhavesh Sharma <buildwithme.ai.dev@gmail.com>
2026-05-13 18:14:28 +0530

    My latest changes

--- src/components/layout/SiteLayout.tsx
+++ src/components/layout/SiteLayout.tsx
@@ -1,12 +1,17 @@
+import type { ReactNode } from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
 
-export const SiteLayout = () => (
+type SiteLayoutProps = {
+  children: ReactNode;
+};
+
+export const SiteLayout = ({ children }: SiteLayoutProps) => (
   <div className="min-h-screen flex flex-col">
     <Navbar />
     <main className="flex-1">
-      <Outlet />
+      {children}
     </main>
     <Footer />
   </div>
